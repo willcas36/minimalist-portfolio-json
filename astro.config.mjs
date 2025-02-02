d322a3143b0a641b6f268195355d5377a4bfb6e1
William Quinones <willcas36@gmail.com>
2025-02-02 14:28:31 -0400

    Adding deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site:'https://willcas36.github.io',
+    base:'/minimalist-portfolio-json'
+});
